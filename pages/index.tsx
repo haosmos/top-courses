f2a4b88472bfbae23e56e0940e2f5c26cedd118d
chaosmos <vitovich1987@gmail.com>
2022-10-28 00:56:04 +0300

    added basic logic for the main page and for the child page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
 import { Htag, Button, P, Tag, Rating } from '../components';
-import { withLayout } from '../components/layout/Layout';
+import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
   
   return (
@@ -25,8 +28,36 @@ function Home(): JSX.Element {
       <Tag size="s" color="primary">Ghost</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
       <Tag size="s" color="red">Ghost</Tag>
+      {menu.map(m => (
+        <li key={m._id.secondCategory}>
+          {m._id.secondCategory}
+        </li>
+      ))}
     </>
   );
 }
 
 export default withLayout(Home);
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number;
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  
+  const { data: menu } = await axios.post<MenuItem[]>(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`,
+    {
+      firstCategory,
+    },
+  );
+  
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
